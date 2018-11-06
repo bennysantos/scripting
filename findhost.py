274662247dc3fbc31d8f8a0955e1803a9b3348b9
bennysantos <44791186+bennysantos@users.noreply.github.com>
2018-11-06 16:22:50 +0000

    Add files via upload

--- findhost.py
+++ findhost.py
@@ -14,16 +14,64 @@ def menu_ports():
 	print("2 - Ports range 0-65535")
 	print("3 - Custom ports range")
 
+def menu_log():
+	print("1 - Print results")
+	print("2 - Save results")
+
+def cp_1_IP(IP):
+	portlist = []
+	os.system("clear")
+	print("Scanning for ports on " + IP + "...\n\n")
+	for port in range(1,1023):
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		result = sock.connect_ex((IP, port))
+		if result == 0:
+			print("Port {}:		OPEN!".format(port))
+			portlist.append(port)
+		else:
+			print("Port {}:		CLOSE!".format(port))
+		sock.close()
+	if len(portlist) == 0:
+		os.system("clear")
+		print("No ports open on " + IP)
+		sys.exit(1)
+	else:
+		while True:
+			os.system("clear")
+			menu_log()
+			opt = input(" > ")
+			if opt == "1":
+				os.system("clear")
+				print("Ports in host " + IP)
+				for port in portlist:
+					print("Port {}:		OPEN!".format(port))
+				print("\n\n")
+				input("Press Enter to Exit")
+				os.system("clear")
+				sys.exit(1)
+			elif opt == "2":
+				os.system("clear")
+				namefile = input("Name to save file > ")
+				file = open(namefile + ".txt","a+")
+				file.write("Ports in host " + IP)
+				for port in portlist:
+					file.write("Port {}:	OPEN!".format(port))
+				file.close()
+				os.system("clear")
+				
+			else:
+				pass
+
 def check_ports_IP(IP):
 	while true:
 		os.system("clear")
 		menu_ports()
 		opt = input(" > ")
-		if opt == 1:
+		if opt == "1":
 			cp_1_IP(IP) #
-		elif opt == 2:
+		elif opt == "2":
 			cp_2_IP(IP) #
-		elif opt == 3:
+		elif opt == "3":
 			while true:
 				custom_cp_1 = input("First port > ")
 				custom_cp_2 = input("Last port > ")
